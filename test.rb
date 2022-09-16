c4a87ac3c8e6318864e662dbaf60713ef7f3cd67
Hieu Nguyen <hieu.nguyenthitrung@gmail.com>
2022-09-16 16:08:26 +0700

    Update test.rb

--- test.rb
+++ test.rb
@@ -2,6 +2,10 @@ class Test
   def initialize
    puts "I love India"
   end
+  
+  def self.test_function(someVar)
+        puts "I got the following variable: " + someVar
+    end
 end
 
 # initialize object
